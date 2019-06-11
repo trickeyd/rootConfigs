f92afa193b3ba57427e90b7ab24f8cd28da138c6
James Trickey <james.trickeyd@gmail.com>
2019-06-11 15:41:44 +0100

    configs added

---
+++ .vim/temp/jsx.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { TextInput, StyleSheet, Text, View, Image } from 'react-native';
+import styles from 'src/client/react/styles';
+
+export default class   extends React.Component {
+    constructor(){
+        super();
+    }
+
+    render() {
+        return (
+
+        )
+    }
+}
