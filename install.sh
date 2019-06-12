3a8244d855bf619773e2f67f6d0012becf185e00
James Trickey <james.trickeyd@gmail.com>
2019-06-12 06:54:54 +0100

    added grep fzf

--- install.sh
+++ install.sh
@@ -9,5 +9,16 @@ sudo apt install build-essential cmake python3-dev
 sudo vim -c 'PluginInstall' -c 'qa!'
 
 cd ~/.vim/bundle/YouCompleteMe
-sudo ython3 install.py --clangd-completer
+sudo python3 install.py --clangd-completer
 
+
+# adds the grep stuff to .bash_profile -  the spaces are required.
+cat <<"EOF" >> ~/.bash_profile
+
+
+# Adds fzf grep functionality
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+bind -x '"\C-p": vim $(fzf);'
+EOF
